77b9b3eadfc7c7b46dc4288b5aa7550ddad64e20
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-09-30 18:39:57 -0500

    Fix: the patient class was debugged

--- VeterinaryCenter.ConsoleApp/Models/Patient.cs
+++ VeterinaryCenter.ConsoleApp/Models/Patient.cs
@@ -3,8 +3,8 @@ namespace VeterinaryCenter.ConsoleApp.Models;
 public class Patient
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public int Age { get; set; }
-    public string Symptom { get; set; }
+    public string Symptom { get; set; } = string.Empty;
 
 }

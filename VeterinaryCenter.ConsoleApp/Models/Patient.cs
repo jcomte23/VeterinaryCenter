a71e013b21cb73c641f4b3ac67bb22cf17c12c96
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-09-30 18:37:59 -0500

    Feat: the patient class was improved

---
+++ VeterinaryCenter.ConsoleApp/Models/Patient.cs
@@ -0,0 +1,10 @@
+namespace VeterinaryCenter.ConsoleApp.Models;
+
+public class Patient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Symptom { get; set; }
+
+}

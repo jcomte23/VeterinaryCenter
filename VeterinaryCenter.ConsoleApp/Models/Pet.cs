5b148d15b8e8ba927f06e23813571b0c5ce3cdbf
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-10-01 17:27:01 -0500

    Feat: the pet model was improved

--- VeterinaryCenter.ConsoleApp/Models/Pet.cs
+++ VeterinaryCenter.ConsoleApp/Models/Pet.cs
@@ -1,9 +1,11 @@
 namespace VeterinaryCenter.ConsoleApp.Models;
 
-public class Pet
+public class Pet(int id, string name, byte ageInMonths, string species, int patientId)
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public byte AgeInMonths { get; set; }
-    public string Species { get; set; } =  string.Empty;
+    public int Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public byte AgeInMonths { get; set; } = ageInMonths;
+    public string Species { get; set; } = species;
+    public int PatientId { get; set; } = patientId;
+    public Patient? Patient { get; set; }
 }

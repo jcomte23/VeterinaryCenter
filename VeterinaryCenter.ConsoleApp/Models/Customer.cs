0db6e5191de48019e30d0ecf401a6947d25d1d8c
javier combita tellez <javiercombita@gmail.com>
2025-10-02 17:02:38 -0500

    Fix: The client class was enhanced with the birthday property and pet list.

--- VeterinaryCenter.ConsoleApp/Models/Customer.cs
+++ VeterinaryCenter.ConsoleApp/Models/Customer.cs
@@ -2,7 +2,9 @@ namespace VeterinaryCenter.ConsoleApp.Models;
 
 internal class Customer : Person
 {
-	internal Customer
+    internal DateOnly BirthDay { get; set; }
+	internal List<Pet> Pets { get; set; }
+    internal Customer
 	(
 		string name,
 		string lastName,
@@ -10,6 +12,11 @@ internal class Customer : Person
 		string documentNumber,
 		string phoneNumber,
 		string email,
-		string address
-	) : base(name, lastName, documentType, documentNumber, phoneNumber, email, address) { }
+		string address,
+		DateOnly birthDay
+	) : base(name, lastName, documentType, documentNumber, phoneNumber, email, address) 
+	{
+		BirthDay = birthDay;
+		Pets = [];
+	}
 }

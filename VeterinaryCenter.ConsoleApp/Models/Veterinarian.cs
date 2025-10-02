427702908d4e3d259a29337089d4b5b3a250faee
javier combita tellez <javiercombita@gmail.com>
2025-10-02 16:56:08 -0500

    Fix: The Person class was implemented through inheritance to the Veterinarian class.

--- VeterinaryCenter.ConsoleApp/Models/Veterinarian.cs
+++ VeterinaryCenter.ConsoleApp/Models/Veterinarian.cs
@@ -1,26 +1,24 @@
 ﻿namespace VeterinaryCenter.ConsoleApp.Models;
 
-internal class Veterinarian
+internal class Veterinarian : Person
 {
-	public Guid Id { get; set; }
-	public string Name { get; set; }
-	public string LastName { get; set; }
-	public DocumentType DocumentType { get; set; }
-	public string DocumentNumber { get; set; }
-	public string PhoneNumber { get; set; }
-	public string Email { get; set; }
-	public string Specialty { get; set; }  // Ej: Cirugía, Vacunas, Medicina General
-	public int YearsExperience { get; set; }
+	internal string Specialty { get; set; }  // Ej: Cirugía, Vacunas, Medicina General
+	internal int YearsExperience { get; set; }
 
-	public Veterinarian(string name, string lastName, DocumentType documentType, string documentNumber, string phoneNumber, string email, string specialty, int yearsExperience)
+	internal Veterinarian
+	(
+		string name, 
+		string lastName, 
+		DocumentType documentType, 
+		string documentNumber, 
+		string phoneNumber, 
+		string email, 
+		string address,
+		string specialty,
+		int yearsExperience
+
+	) : base(name, lastName, documentType, documentNumber, phoneNumber, email, address)
 	{
-		Id = Guid.NewGuid();
-		Name = name;
-		LastName = lastName;
-		DocumentType = documentType;
-		DocumentNumber = documentNumber;
-		PhoneNumber = phoneNumber;
-		Email = email;
 		Specialty = specialty;
 		YearsExperience = yearsExperience;
 	}

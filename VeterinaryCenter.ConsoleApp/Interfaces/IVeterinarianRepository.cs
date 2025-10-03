c0063ec5e45cf8902eb359d0ee0da976b712fcda
javier combita tellez <javiercombita@gmail.com>
2025-10-02 19:44:13 -0500

    Feat: The repository pattern is implemented in veterinarian

---
+++ VeterinaryCenter.ConsoleApp/Interfaces/IVeterinarianRepository.cs
@@ -0,0 +1,12 @@
+﻿using VeterinaryCenter.ConsoleApp.Models;
+
+namespace VeterinaryCenter.ConsoleApp.Interfaces;
+
+internal interface IVeterinarianRepository
+{
+	void AddVeterinarian(Veterinarian veterinarian);
+	List<Veterinarian> GetAllVeterinarians();
+	Veterinarian? GetVeterinarianById(Guid id);
+	void UpdateVeterinarian(Veterinarian veterinarian);
+	void DeleteVeterinarian(Guid id);
+}
---
+++ VeterinaryCenter.ConsoleApp/Repositories/VeterinarianRepository.cs
@@ -0,0 +1,33 @@
+﻿using VeterinaryCenter.ConsoleApp.Interfaces;
+using VeterinaryCenter.ConsoleApp.Models;
+
+namespace VeterinaryCenter.ConsoleApp.Repositories;
+
+internal class VeterinarianRepository : IVeterinarianRepository
+{
+    public void AddVeterinarian(Veterinarian veterinarian)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteVeterinarian(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Veterinarian> GetAllVeterinarians()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Veterinarian? GetVeterinarianById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateVeterinarian(Veterinarian veterinarian)
+    {
+        throw new NotImplementedException();
+    }
+}
+

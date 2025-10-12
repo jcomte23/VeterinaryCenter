de068d18d4c2875903d1b38fd3fcad355b60381e
javier combita tellez <javiercombita@gmail.com>
2025-10-12 18:51:50 -0500

    Fix: The entire repository system for animals was created

---
+++ VeterinaryCenter.ConsoleApp/Interfaces/IAnimalRepository.cs
@@ -0,0 +1,15 @@
+﻿using VeterinaryCenter.ConsoleApp.Entities;
+using VeterinaryCenter.ConsoleApp.Models;
+
+namespace VeterinaryCenter.ConsoleApp.Interfaces;
+
+internal interface IAnimalRepository
+{
+    void AddAnimal(Animal animal);
+    List<Animal> GetAllAnimals();
+    Animal? GetAnimalById(Guid id);
+    void UpdateAnimal(Animal animal);
+    void DeleteAnimal(Guid id);
+    List<Animal> GetAnimalsByOwner(Customer owner);
+    List<Animal> GetAnimalsBySpecies(string species);
+}
---
+++ VeterinaryCenter.ConsoleApp/Repositories/AnimalRepository.cs
@@ -0,0 +1,48 @@
+﻿using VeterinaryCenter.ConsoleApp.Data;
+using VeterinaryCenter.ConsoleApp.Entities;
+using VeterinaryCenter.ConsoleApp.Interfaces;
+using VeterinaryCenter.ConsoleApp.Models;
+
+namespace VeterinaryCenter.ConsoleApp.Repositories;
+
+internal class AnimalRepository : IAnimalRepository
+{
+    public void AddAnimal(Animal animal)
+    {
+        Database.Animals.Add(animal);
+    }
+
+    public List<Animal> GetAllAnimals()
+    {
+        return [.. Database.Animals];
+    }
+
+    public Animal? GetAnimalById(Guid id)
+    {
+        return Database.Animals.FirstOrDefault(a => a.Id == id);
+    }
+
+    public void UpdateAnimal(Animal animal)
+    {
+        var existing = GetAnimalById(animal.Id) ?? throw new KeyNotFoundException($"No se encontró el animal con ID {animal.Id}");
+        var index = Database.Animals.IndexOf(existing);
+        Database.Animals[index] = animal;
+    }
+
+    public void DeleteAnimal(Guid id)
+    {
+        var animal = GetAnimalById(id);
+        if (animal is not null)
+            Database.Animals.Remove(animal);
+    }
+
+    public List<Animal> GetAnimalsByOwner(Customer owner)
+    {
+        return [.. Database.Animals.Where(a => a.Owner?.Id == owner.Id)];
+    }
+
+    public List<Animal> GetAnimalsBySpecies(string species)
+    {
+        return [.. Database.Animals.Where(a => a.Species.Equals(species, StringComparison.OrdinalIgnoreCase))];
+    }
+}
